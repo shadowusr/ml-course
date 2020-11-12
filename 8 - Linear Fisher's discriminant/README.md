8e709725822a668e735932c7ca28fa64431b19cc
shadow <nikhakerlab@gmail.com>
2020-11-13 01:08:08 +0300

    Implemented task 8

---
+++ 8 - Linear Fisher's discriminant/README.md
@@ -0,0 +1,86 @@
+# 8 - Linear Fisher's discriminant
+### Assignment objective
+- Implement linear Fisher's discriminant
+- Make a plot showing the algorithm in action
+- Point on the method's advantages in comparison to the plug-in algorithm
+
+### Assignment implementation
+###### Theory
+Before, we assumed that conditional probability density functions are normally distributed with mean and covariance parameters. Now we make the additional simplifying assumption that the class covariance matrices are all the same. Thus we increase the number of objects used to compute the covariance matrix, make it more stable along with simplifying the learning algorithm. The borderline of this method is always a straight line. It generalizes well on datasets where classes have clear borders and simple shape, but fails in cases when classes have some particular curvatures in their shape.
+
+The "average" covariance matrix over the whole dataset is computed according to the following formulae: 
+<a href="https://www.codecogs.com/eqnedit.php?latex=\Sigma=\frac{1}{l&space;-&space;|Y|}\sum^l_{i&space;=&space;1}(x_i&space;-&space;\mu_{y_i})(x_i&space;-&space;\mu_{y_i})^T" target="_blank"><img src="https://latex.codecogs.com/gif.latex?\Sigma=\frac{1}{l&space;-&space;|Y|}\sum^l_{i&space;=&space;1}(x_i&space;-&space;\mu_{y_i})(x_i&space;-&space;\mu_{y_i})^T" title="\Sigma=\frac{1}{l - |Y|}\sum^l_{i = 1}(x_i - \mu_{y_i})(x_i - \mu_{y_i})^T" /></a>
+
+###### Classification map examples
+| ![](https://i.imgur.com/0I16ml8.png) | ![](https://i.imgur.com/ob6buHx.png) |
+| - | - |
+
+###### Comparison to the plug-in algorithm & method's advantages
+| ![](https://i.imgur.com/g3s6ApT.png) | ![](https://i.imgur.com/tKVoRjC.png) |
+| - | - |
+| Linear discriminant of Fisher | Plug-in algorithm |
+
+The fact that we compute the covariance matrix over the whole dataset makes the algorithm more stable, while keeping the learning algorithm relatively simple. It's well known that the simpler model is the better it generalizes on real world data, and LDF does exactly that.
+
+###### Code
+```R
+getPriorProbabilities <- function (dset, column) {
+  dsetLength <- dim(dset)[1]
+  return (table(dset[, column]) / dsetLength)
+}
+
+getCovMatrix <- function(dset, expectation) {
+  featureTypesCount <- dim(dset)[2]
+  dsetLength <- dim(dset)[1]
+  cov <- matrix(0, nrow = featureTypesCount - 1, ncol = featureTypesCount - 1)
+  
+  for (i in 1:dsetLength) {
+    currentFeature <- as.matrix(dset[i, 1:featureTypesCount - 1], nrow = 1)
+
+    tmp <- t(currentFeature - expectation) %*% (currentFeature - expectation)
+    cov <- cov + tmp
+  }
+  cov <- cov / (dsetLength - 1)
+  
+  return(cov)
+}
+
+getLikelihood <- function(point, expectation, covMatrix) {
+  covMatrixInverse <- solve(covMatrix)
+  a <- -(t(expectation) %*% covMatrixInverse %*% expectation) / 2
+  b <- t(point) %*% covMatrixInverse %*% expectation
+  
+  return(a + b)
+}
+
+getLdfProbability <- function(dset, classesColumn, className, point, lambda, covMatrix, expectations) {
+  featuresCount <- length(point)
+  croppedDset <- dset[which(dset[, classesColumn] == className), ]
+  dsetLength <- dim(dset)[1]
+  
+  prior <- getPriorProbabilities(dset, classesColumn)[className]
+
+  # lambda = rep(1, featuresCount)
+  likelihood <- getLikelihood(point, expectations, covMatrix)
+  
+  probability <- log(lambda * prior) + likelihood;
+  return (probability)
+}
+
+ldfClassifier <- function(point, dset) {
+  classes <- unique(dset[, 3])
+  classesCount <- length(classes)
+  scores <- array(dim = classesCount)
+  
+  expectations <- array(dim = featuresCount)
+  for (i in 1:featuresCount) {
+    expectations[i] <- mean(croppedDset[, i])
+  }
+  covMatrix <- getCovMatrix(dset, matrix(expectations, nrow=1))
+  for (i in 1:classesCount) {
+    scores[i] <- getPlugInProbability(dset, 'class', classes[i], point, lambdas[i], covMatrix, expectations)
+  }
+  # print(scores)
+  return(classes[which.max(scores)])
+}
+```
---
+++ 8 - Linear Fisher's discriminant/ldf.r
@@ -0,0 +1,121 @@
+library(MASS)
+getPriorProbabilities <- function (dset, column) {
+  dsetLength <- dim(dset)[1]
+  return (table(dset[, column]) / dsetLength)
+}
+
+getCovMatrix <- function(dset, expectation) {
+  featureTypesCount <- dim(dset)[2]
+  dsetLength <- dim(dset)[1]
+  cov <- matrix(0, nrow = featureTypesCount - 1, ncol = featureTypesCount - 1)
+  
+  for (i in 1:dsetLength) {
+    currentFeature <- as.matrix(dset[i, 1:featureTypesCount - 1], nrow = 1)
+
+    tmp <- t(currentFeature - expectation) %*% (currentFeature - expectation)
+    cov <- cov + tmp
+  }
+  cov <- cov / (dsetLength - 1)
+  
+  return(cov)
+}
+
+getLikelihood <- function(point, expectation, covMatrix) {
+  covMatrixInverse <- solve(covMatrix)
+  a <- -(t(expectation) %*% covMatrixInverse %*% expectation) / 2
+  b <- t(point) %*% covMatrixInverse %*% expectation
+  
+  return(a + b)
+}
+
+getLdfProbability <- function(dset, classesColumn, className, point, lambda, covMatrix, expectations) {
+  featuresCount <- length(point)
+  croppedDset <- dset[which(dset[, classesColumn] == className), ]
+  dsetLength <- dim(dset)[1]
+  
+  prior <- getPriorProbabilities(dset, classesColumn)[className]
+
+  # lambda = rep(1, featuresCount)
+  likelihood <- getLikelihood(point, expectations, covMatrix)
+  
+  probability <- log(lambda * prior) + likelihood;
+  return (probability)
+}
+
+ldfClassifier <- function(point, dset) {
+  classes <- unique(dset[, 3])
+  classesCount <- length(classes)
+  scores <- array(dim = classesCount)
+  
+  expectations <- array(dim = featuresCount)
+  for (i in 1:featuresCount) {
+    expectations[i] <- mean(croppedDset[, i])
+  }
+  covMatrix <- getCovMatrix(dset, matrix(expectations, nrow=1))
+  for (i in 1:classesCount) {
+    scores[i] <- getPlugInProbability(dset, 'class', classes[i], point, lambdas[i], covMatrix, expectations)
+  }
+  # print(scores)
+  return(classes[which.max(scores)])
+}
+
+# mu1 <- c(0, 0)
+# mu2 <- c(4, 4)
+# sig1 <- matrix(c(2, 0.9, 0.9, 2), 2, 2)
+# sig2 <- matrix(c(0.5, 0, 0, 2), 2, 2)
+# dset1 <- mvrnorm(250, mu1, sig1)
+# dset2 <- mvrnorm(250, mu2, sig2)
+# dset <- rbind(cbind(dset1, 1),cbind(dset2, 2))
+
+dset = data.frame(x = double(), y = double(), class = character())
+
+dset <- rbind(dset, data.frame(x = 0, y = 2, class = 1))
+dset <- rbind(dset, data.frame(x = 2, y = 0, class = 1))
+dset <- rbind(dset, data.frame(x = 2.5, y = 2.5, class = 1))
+dset <- rbind(dset, data.frame(x = 2.8, y = 2.8, class = 1))
+
+dset <- rbind(dset, data.frame(x = 2, y = 4.5, class = 2))
+dset <- rbind(dset, data.frame(x = 4.5, y = 2, class = 2))
+dset <- rbind(dset, data.frame(x = 6, y = 6, class = 2))
+dset <- rbind(dset, data.frame(x = 6, y = 5, class = 2))
+dset <- rbind(dset, data.frame(x = 5, y = 6, class = 2))
+dset <- rbind(dset, data.frame(x = 3, y = 5.5, class = 2))
+dset <- rbind(dset, data.frame(x = 5.5, y = 3, class = 2))
+
+# 
+# dset <- rbind(dset, data.frame(x = 2, y = 0, class = 1))
+# dset <- rbind(dset, data.frame(x = 0, y = 2, class = 1))
+# dset <- rbind(dset, data.frame(x = 1.3, y = 0.7, class = 1))
+# dset <- rbind(dset, data.frame(x = 0.8, y = 1.5, class = 1))
+# dset <- rbind(dset, data.frame(x = 0.9, y = 1.8, class = 1))
+# dset <- rbind(dset, data.frame(x = 1.7, y = 0.8, class = 1))
+# 
+# dset <- rbind(dset, data.frame(x = 2, y = 2, class = 2))
+# dset <- rbind(dset, data.frame(x = 0, y = 0, class = 2))
+# dset <- rbind(dset, data.frame(x = 1.7, y = 1.9, class = 2))
+# dset <- rbind(dset, data.frame(x = 0.3, y = 0.1, class = 2))
+
+plot(dset[ ,1], dset[ ,2], pch=21, bg=c("green","blue")[dset[ ,3]])
+
+# print(plugInClassifier(c(0, 0), dset))
+# # print(getPlugInProbability(dset, 3, 1, c(0, 0), 1))
+
+## generating map
+colors <- c("1" = "green", "2" = "blue")
+xs <- seq(from = -5, to = 7, by = 0.2)
+ys <- seq(from = -5, to = 8, by = 0.2)
+## lambdas <- runif(2, min=1, max=100)
+lambdas = c(1, 1)
+print(lambdas)
+
+all <- length(xs) * length(ys)
+progress = 1
+for (i in xs) {
+  for (j in ys) {
+    cat("\rProcessing point", progress, "of", all)
+    progress <- progress + 1
+    result <- plugInClassifier(c(i, j), dset)
+    points(i, j, col = colors[result], pch = 21)
+  }
+}
+cat("\rDone.                      \n")
